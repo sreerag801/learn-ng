c9f732675d282f9b3034deb67f1373299aa2186f
coder-maniac <sreerag801@outlook.com>
2024-01-27 21:11:38 +0530

    basic implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { AppCardComponent, CardBodyDirective, CardHeaderDirective, CardMainDirective } from './app-card/app-card.component';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppCardComponent, CardHeaderDirective, CardBodyDirective, CardMainDirective],
+  imports: [RouterOutlet, ReactiveFormsModule, NgSwitch, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  type!: 'text' | 'select';
 
   constructor(private fb: FormBuilder){}
 
   
 
   ngOnInit(): void {
-    
+    this.type = 'select'
   }
 
   
