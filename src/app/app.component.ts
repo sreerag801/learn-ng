719d8e19c787e6b103f1edfcc35831f8eeff045e
coder-maniac <sreerag801@outlook.com>
2024-01-27 15:58:36 +0530

    learn custome validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { JsonPipe, NgFor } from '@angular/common';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { AddressComponent } from './address/address.component';
 import { BesicInfoComponent } from './besic-info/besic-info.component';
@@ -8,7 +8,7 @@ import { BesicInfoComponent } from './besic-info/besic-info.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, NgFor, ReactiveFormsModule, AddressComponent, BesicInfoComponent],
+  imports: [RouterOutlet, JsonPipe, NgFor, ReactiveFormsModule, AddressComponent, BesicInfoComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,27 +17,31 @@ export class AppComponent implements OnInit {
 
   constructor(private fb: FormBuilder){}
 
-  get colorsList(): FormArray {
-    return this.myForm.get('colors') as FormArray;
-  }
+  
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      colors: this.fb.array([
-        this.fb.group({name: 'red'}),
-        this.fb.group({name: 'green'}),
-        this.fb.group({name: 'yellow'}),
-        this.fb.group({name: 'orange'}),
-        this.fb.group({name: 'purple'})
-      ])
-    })
+      password: this.fb.control(null, [Validators.required, Validators.min(2)]),
+      confirmPassword: this.fb.control(null, [Validators.min(2)])},
+    { validators: [this.valuesAreEqual('password', 'confirmPassword')]})
   }
 
-  remove(index: number){
-    this.colorsList.removeAt(index);
+  private valuesAreEqual(
+    controlNameA: string,
+    controlNameB: string
+  ): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+
+      const fromGroup = control as FormGroup;
+      let password = fromGroup.get(controlNameA)?.value;
+      let confirmPassword = fromGroup.get(controlNameB)?.value;
+      if(password === confirmPassword){
+        return null;
+      }
+      else{
+        return {notMatched: true}
+      }
+    }
   }
 
-  AddColor(){
-    this.colorsList.push(this.fb.group({name: ''}));
-  }
 }
