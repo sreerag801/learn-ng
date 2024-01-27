736b3dce9f077bf43389e08754ee2183ff41229c
coder-maniac <sreerag801@outlook.com>
2024-01-27 21:23:55 +0530

    Angular Dynamic Form - step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { AppCardComponent, CardBodyDirective, CardHeaderDirective, CardMainDirective } from './app-card/app-card.component';
-import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { JsonPipe, NgSwitch, NgSwitchCase } from '@angular/common';
 import { IDynamicFieldModel } from './model/dynamicFieldModel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, NgSwitch, NgSwitchCase],
+  imports: [RouterOutlet, ReactiveFormsModule, NgSwitch, NgSwitchCase, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   dynamicField!: IDynamicFieldModel
+  myForm!: FormGroup;
 
   constructor(private fb: FormBuilder){}
 
@@ -24,6 +25,10 @@ export class AppComponent implements OnInit {
       type: 'select',
       label: 'Please select value'
     }
+
+    this.myForm = this.fb.group({
+      dynamicFields: []
+    })
   }
 
   
