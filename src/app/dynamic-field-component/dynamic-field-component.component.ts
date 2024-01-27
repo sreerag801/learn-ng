aab4ec58dbd519875d7ab62e2856d9f1345184de
sreerag801 <44240411+sreerag801@users.noreply.github.com>
2024-01-28 00:26:23 +0530

    Ng dynamic form (#6)
    * dynamic form select menu option added
    
    * Angular Dynamic Form - with validators
    
    * validator added via validatorFun
    
    * integrated api services
    
    * playing with code
    
    ---------

--- src/app/dynamic-field-component/dynamic-field-component.component.ts
+++ src/app/dynamic-field-component/dynamic-field-component.component.ts
@@ -1,12 +1,13 @@
-import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { KeyValuePipe, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 import { IDynamicFieldModel } from '../model/dynamicFieldModel';
+import { DataServiceService } from '../service/data-service.service';
 
 @Component({
   selector: 'app-dynamic-field-component',
   standalone: true,
-  imports: [NgSwitch, NgSwitchCase, ReactiveFormsModule],
+  imports: [NgSwitch, NgSwitchCase, ReactiveFormsModule, KeyValuePipe, NgFor, NgIf],
   templateUrl: './dynamic-field-component.component.html',
   styleUrl: './dynamic-field-component.component.css'
 })
@@ -15,7 +16,17 @@ export class DynamicFieldComponentComponent {
   form!:  FormGroup;
 
 
-  constructor(private rootFormGroup: FormGroupDirective){
+  constructor(private rootFormGroup: FormGroupDirective,
+    private api: DataServiceService){
     this.form = this.rootFormGroup.control as FormGroup;
+  }
+
+  addNewDropDownItem(formItem: IDynamicFieldModel){
+
+    this.api
+        .addItemToDropDown('randon'+ Math.random().toString(), Math.random().toString())
+        .subscribe(data=>{
+            formItem.selectMenuOpton = data;
+        });
   }
 }
--- src/app/model/dynamicFieldModel.ts
+++ src/app/model/dynamicFieldModel.ts
@@ -1,5 +1,11 @@
+import { ValidatorFn } from "@angular/forms";
+import { IDropDownMenuOption } from "../service/data-service.service";
+
 export interface IDynamicFieldModel {
  id: string;
- type: 'text' | 'select';
- label: string
+ type: 'text' | 'select' | 'number';
+ label: string;
+ selectMenuOpton?: IDropDownMenuOption;
+ value?: string | number;
+ validators?: ValidatorFn[]
 }
