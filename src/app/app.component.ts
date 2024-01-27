3f3744128eefbb51ec8cb5f73b295a9dd1baebf4
coder-maniac <sreerag801@outlook.com>
2024-01-27 13:06:57 +0530

    learning drop down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { JsonPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, NgFor, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learn-angular';
+  items = [
+    {key: 0, value: '---Select--'},
+    {key: 1, value: 'India'},
+    {key: 2, value: 'USA'},
+    {key: 3, value: 'Russia'}
+  ];
+
+  selectedItem = new FormControl(this.items[1]);
 }
